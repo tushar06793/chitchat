11e991991efc3df6a88870c4f85ef862594c4051
tushar06793 <tushar06793@gmail.com>
2021-12-24 02:27:26 +0530

    settings Screen

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,11 +1,22 @@
 import 'package:chitchat/models/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   final LocalUser user;
   final bool isAdmin;
   SettingsScreen({required this.user, required this.isAdmin});
+
+  @override
+  _SettingsScreenState createState() =>
+      _SettingsScreenState(user: user, isAdmin: isAdmin);
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  final LocalUser user;
+  final bool isAdmin;
+  _SettingsScreenState({required this.user, required this.isAdmin});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
