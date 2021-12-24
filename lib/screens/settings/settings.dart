65e072ea42d15e1fbb12c5d104f9411528a3b39a
tushar06793 <tushar06793@gmail.com>
2021-12-24 16:34:09 +0530

    Profile Photo feature

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
 import 'package:chitchat/models/user.dart';
 import 'package:chitchat/services/firebase_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
 
 class SettingsScreen extends StatefulWidget {
   final LocalUser user;
@@ -19,9 +25,32 @@ class _SettingsScreenState extends State<SettingsScreen> {
   final bool isAdmin;
 
   final _newUsername = TextEditingController();
-
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final Service service = Service();
-  
+
+  File? imageFile;
+
+  Future getImage() async {
+    ImagePicker _picker = ImagePicker();
+
+    await _picker.pickImage(source: ImageSource.gallery).then((xFile) {
+      if (xFile != null) {
+        imageFile = File(xFile.path);
+        uploadProfile();
+      }
+    });
+  }
+
+  Future uploadProfile() async {
+    String filename = Uuid().v1();
+    var ref =
+        FirebaseStorage.instance.ref().child('images').child("$filename.jpg");
+    var uploadTask = await ref.putFile(imageFile!);
+    String imageUrl = await uploadTask.ref.getDownloadURL();
+
+    print(imageUrl);
+  }
+
   _SettingsScreenState({required this.user, required this.isAdmin});
 
   @override
@@ -30,56 +59,98 @@ class _SettingsScreenState extends State<SettingsScreen> {
       appBar: AppBar(
         title: Text("Settings"),
       ),
-      body: ListTile(
-        contentPadding: EdgeInsets.only(left: 10, right: 7),
-        leading: CircleAvatar(
-          backgroundImage: AssetImage(user.profile),
-        ),
-        title: Text(
-          user.username,
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        subtitle: Text("Hey There! I am using whatsApp"),
-        trailing: IconButton(
-            onPressed: () async {
-              showDialog(
-                  context: context,
-                  barrierDismissible: false,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: Text('Edit Name'),
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          TextField(
-                            controller: _newUsername,
-                            keyboardType: TextInputType.text,
-                          )
-                        ],
-                      ),
-                      actions: <Widget>[
-                        FlatButton(
-                          onPressed: () async {
-                            Navigator.pop(context);
-                          },
-                          child: Text('Back'),
-                          textColor: Colors.white,
-                          color: Colors.blue,
-                        ),
-                        FlatButton(
-                          onPressed: () async {
-                            user.username = _newUsername.text.trim();
-                            await service.updateName(user);
-                          },
-                          child: Text('Change'),
-                          textColor: Colors.white,
-                          color: Colors.blue,
-                        )
-                      ],
-                    );
-                  });
-            },
-            icon: Icon(Icons.edit)),
+      body: Column(
+        children: [
+          Padding(padding: const EdgeInsets.only(top: 10.0)),
+          SizedBox(
+            height: 200,
+            child: Stack(
+              children: [
+                Padding(padding: const EdgeInsets.only(top: 10.0)),
+                CircleAvatar(
+                  backgroundImage: AssetImage(user.profile),
+                  radius: 90,
+                ),
+                Positioned(
+                  right: 10,
+                  top: 130,
+                  child: IconButton(
+                      onPressed: () => getImage(),
+                      icon: Icon(
+                        Icons.camera_alt,
+                        size: 40,
+                      )),
+                )
+              ],
+            ),
+          ),
+          ListTile(
+            contentPadding: EdgeInsets.only(left: 10, right: 7),
+            leading: Icon(
+              Icons.account_box,
+              size: 40,
+            ),
+            title: Text(
+              "Name",
+            ),
+            subtitle: Text(user.username,
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            trailing: IconButton(
+                onPressed: () async {
+                  showDialog(
+                      context: context,
+                      barrierDismissible: false,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text('Edit Name'),
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              TextField(
+                                controller: _newUsername,
+                                keyboardType: TextInputType.text,
+                              )
+                            ],
+                          ),
+                          actions: <Widget>[
+                            FlatButton(
+                              onPressed: () async {
+                                Navigator.pop(context);
+                              },
+                              child: Text('Back'),
+                              textColor: Colors.white,
+                              color: Colors.blue,
+                            ),
+                            FlatButton(
+                              onPressed: () async {
+                                user.username = _newUsername.text.trim();
+                                await service.updateName(user);
+                              },
+                              child: Text('Change'),
+                              textColor: Colors.white,
+                              color: Colors.blue,
+                            )
+                          ],
+                        );
+                      });
+                },
+                icon: Icon(Icons.edit)),
+          ),
+          ListTile(
+            contentPadding: EdgeInsets.only(left: 10, right: 7),
+            leading: Icon(
+              Icons.call,
+              size: 35,
+            ),
+            title: Text(
+              "Phone",
+            ),
+            subtitle: Text(
+              user.phone,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
       ),
     );
   }
